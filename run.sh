7602a771691a39f3499248f3f94ff46c1a34c116
mshsvnv <mshsvnv@mail.ru>
2026-06-16 17:58:18 +0300

    [*]: some fixes

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-py.exe -m pip install -r req.txt
+python -m pip install -r req.txt
 
 echo "Generate flow..."
-# py.exe generate_flow.py
+# python generate_flow.py
 
 MODELS=("IFRNet" "IFRNet_RGB2")
 
@@ -13,13 +13,13 @@ for MODEL in "${MODELS[@]}"; do
 
     echo "Starting experiment for: $MODEL"
 
-    py.exe train.py --model_name "$MODEL" --epochs 100 --batch_size 2 --lr_start 1e-4 --lr_end 1e-5
+    python train.py --model_name "$MODEL" --epochs 100 --batch_size 2 --lr_start 1e-4 --lr_end 1e-5
 
     echo "  Benchmarking $MODEL..."
-    py.exe benchmarks/speed_parameters.py --model_name "$MODEL"> "./checkpoint/${MODEL}/speed_parameters.txt"
+    python benchmarks/speed_parameters.py --model_name "$MODEL"> "./checkpoint/${MODEL}/speed_parameters.txt"
 
     echo "  Evaluating $MODEL..."
-    py.exe benchmarks/eval_vimeo90k.py --model_name "$MODEL" > "./checkpoint/${MODEL}/eval.txt"
+    python benchmarks/eval_vimeo90k.py --model_name "$MODEL" > "./checkpoint/${MODEL}/eval.txt"
 
     echo "Finished experiment for: $MODEL"
 done
